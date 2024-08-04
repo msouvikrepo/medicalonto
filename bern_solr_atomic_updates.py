3fcfcd2e1657b6687c5bdaadffc24a0e95696e86
Souvik Mondal <souvikmondal15@gmail.com>
2024-08-04 03:47:53 +0200

    chore : removed solr update script

--- bern_solr_atomic_updates.py
+++
@@ -1,7 +0,0 @@
-import os, json
-import pandas as pd
-
-
-path_to_json = 'test_files/'
-json_files = [pos_json for pos_json in os.listdir(path_to_json) if pos_json.endswith('.json')]
-print(json_files)
